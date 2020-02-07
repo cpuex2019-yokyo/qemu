13a10206b20985b40a6521e4c37c0178c2468cf6
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2020-02-07 10:19:15 +0900

    fix uart mem map

--- hw/riscv/virt.c
+++ hw/riscv/virt.c
@@ -576,7 +576,7 @@ static void riscv_virt_board_init(MachineState *machine)
                          DEVICE(s->plic), true);
 
     serial_mm_init(system_memory, memmap[VIRT_UART0].base,
-        0, qdev_get_gpio_in(DEVICE(s->plic), UART0_IRQ), 399193,
+        2, qdev_get_gpio_in(DEVICE(s->plic), UART0_IRQ), 399193,
         serial_hd(0), DEVICE_LITTLE_ENDIAN);
 
     virt_flash_create(s);
